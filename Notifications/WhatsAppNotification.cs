4c55b312f022e070d8c7053b3ff513cafca50f58
Shady Mahmoud <120749934+Shady129@users.noreply.github.com>
2026-06-07 09:53:47 +0300

    Update WhatsAppNotification.cs

--- Notifications/WhatsAppNotification.cs
+++ Notifications/WhatsAppNotification.cs
@@ -1,1 +1,12 @@
+namespace DelegateNotificationOOP
+{
+    public class WhatsAppNotification:INotification
+    {
+        public void Send(string title, string message)
+        {
+            Console.WriteLine($"WhatsApp: {title} - {message}");
+        }
+
+    }
+}
 
