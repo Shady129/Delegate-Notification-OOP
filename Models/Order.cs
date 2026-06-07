4bd9d44d5021b322a8c528ab1d560d86551040ef
Shady Mahmoud <120749934+Shady129@users.noreply.github.com>
2026-06-07 10:06:49 +0300

    Update Order.cs

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,1 +1,20 @@
+namespace DelegateNotificationOOP
+{
+    public class Order
+    {
+        Notify OrderNotifier;
+
+
+        public Order(Notify myNotify)
+        {
+            OrderNotifier = myNotify;
+        }
+
+
+        public void CreateOrder()
+        {
+            OrderNotifier?.Invoke("Order Created", "Your order has been created successfully");
+        }
+    }
+}
 
