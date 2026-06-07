4a778049555b10396c26d8587b3dac7cd1db60b3
Shady Mahmoud <120749934+Shady129@users.noreply.github.com>
2026-06-07 09:48:16 +0300

    Update SmsNotification.cs

--- Notifications/SmsNotification.cs
+++ Notifications/SmsNotification.cs
@@ -1,1 +1,13 @@
+namespace DelegateNotificationOOP
+{
+    public class SmsNotification:INotification
+    {
+
+        public void Send(string title, string message)
+        {
+            Console.WriteLine($"SMS: {title} - {message}");
+        }
+    }
+
+}
 
