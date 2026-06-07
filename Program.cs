777ad63b7916494995c59c1d6df487d8f20f11da
Shady Mahmoud <120749934+Shady129@users.noreply.github.com>
2026-06-07 10:00:13 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,36 @@
+namespace DelegateNotificationOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Notify myNotify;
+
+
+            INotification notification = new SmsNotification();
+            myNotify = notification.Send;
+
+
+            notification = new EmailNotification();
+            myNotify += notification.Send;
+
+
+
+            notification = new PushNotification();
+            myNotify += notification.Send;
+
+
+            notification = new WhatsAppNotification();
+            myNotify += notification.Send;
+
+
+
+            Order myOrder = new Order(myNotify);
+
+            myOrder.CreateOrder();
+
+        }
+    }
+}
 
