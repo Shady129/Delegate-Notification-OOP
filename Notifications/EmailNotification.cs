6e02033910efb1183d3fb913bedea76815d6655a
Shady Mahmoud <120749934+Shady129@users.noreply.github.com>
2026-06-07 09:49:14 +0300

    Update EmailNotification.cs

--- Notifications/EmailNotification.cs
+++ Notifications/EmailNotification.cs
@@ -1,1 +1,13 @@
+namespace DelegateNotificationOOP
+{
+    public class EmailNotification: INotification
+    {
+
+        public void Send(string title, string message)
+        {
+            Console.WriteLine($"Email: {title} - {message}");
+        }
+
+    }
+}
 
