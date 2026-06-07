d8542e25f02185dcf9377088d1725755ce9d064d
Shady Mahmoud <120749934+Shady129@users.noreply.github.com>
2026-06-07 09:50:25 +0300

    Update PushNotification.cs

--- Notifications/PushNotification.cs
+++ Notifications/PushNotification.cs
@@ -1,1 +1,11 @@
+namespace DelegateNotificationOOP
+{
+    public class PushNotification:INotification
+    {
+        public void Send(string title, string message)
+        {
+            Console.WriteLine($"Push: {title} - {message}");
+        }
+    }
+}
 
